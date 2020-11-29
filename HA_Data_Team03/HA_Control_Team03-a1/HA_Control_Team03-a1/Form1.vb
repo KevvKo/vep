6efb0febc4fccdab640d3d22963b70d49d82e985
KevvKo <kklein915@gmail.com>
2020-11-29 23:53:52 +0100

    init repo with first 2 tasks

---
+++ HA_Data_Team03/HA_Control_Team03-a1/HA_Control_Team03-a1/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim selectedIndex As String = ComboBox1.SelectedIndex
+        RichTextBox1.Text = ""
+
+        If (selectedIndex = 0) Then
+
+            RichTextBox1.Text &= "For ... Next: " & vbNewLine & vbNewLine
+            For i As Integer = 1 To 100
+                If primenumber(i) Then
+                    RichTextBox1.Text &= i & vbNewLine
+                End If
+            Next i
+        End If
+
+        If (selectedIndex = 1) Then
+            Dim i As Integer = 1
+            RichTextBox1.Text &= "Do While ... Loop: " & vbNewLine & vbNewLine
+
+            Do While i <= 100
+                If primenumber(i) Then
+                    RichTextBox1.Text &= i & vbNewLine
+                End If
+                i += 1
+            Loop
+        End If
+
+        If (selectedIndex = 2) Then
+            Dim i As Integer = 1
+            RichTextBox1.Text &= "Do ... Loop While: " & vbNewLine & vbNewLine
+            Do
+                If primenumber(i) Then
+                    RichTextBox1.Text &= i & vbNewLine
+                End If
+                i += 1
+            Loop While i <= 100
+        End If
+
+        If (selectedIndex = 3) Then
+            Dim i As Integer = 1
+            RichTextBox1.Text &= "Do Until ... Loop: " & vbNewLine & vbNewLine
+
+            Do Until i > 100
+                If primenumber(i) Then
+                    RichTextBox1.Text &= i & vbNewLine
+                End If
+                i += 1
+            Loop
+        End If
+
+        If (selectedIndex = 4) Then
+            Dim i As Integer = 1
+            RichTextBox1.Text &= "Do ... Loop Until: " & vbNewLine & vbNewLine
+
+            Do
+                If primenumber(i) Then
+                    RichTextBox1.Text &= i & vbNewLine
+                End If
+                i += 1
+            Loop Until i > 100
+        End If
+
+    End Sub
+
+    Function primenumber(x As Integer)
+        Dim isPrim As Boolean = True
+        For i As Integer = 2 To x
+            If x Mod i = 0 And i <> x Then
+                isPrim = False
+                Exit For
+            End If
+        Next i
+
+        Return x > 1 And isPrim
+
+
+    End Function
+
+End Class
---
+++ HA_Data_Team08/übung1/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Structure User
+
+        Dim firstName, lastName, fullName, street, streetNumber, postcode, town As String
+        Dim birthday As Date
+        Dim age As Integer
+        Dim legalAge As Boolean
+
+    End Structure
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles getData.Click
+
+        Dim user As User = New User()
+
+        user.firstName = Interaction.InputBox("Vorname")
+        user.lastName = Interaction.InputBox("Nachname")
+        user.fullName = user.firstName + " " + user.lastName
+
+        user.street = Interaction.InputBox("Straße")
+        user.streetNumber = Interaction.InputBox("Hausnummer")
+        user.postcode = Interaction.InputBox("PLZ")
+        user.town = Interaction.InputBox("Ort")
+        user.birthday = Convert.ToDateTime(Interaction.InputBox("Geburtstag"))
+
+        user.age = GetCurrentAge(user.birthday)
+        user.legalAge = user.age >= 18
+
+        Dim dayOfYear As String = New Date(user.birthday.Year, user.birthday.Month, user.birthday.Day).DayOfYear().ToString
+
+        'fills all labels with the nessecary strings
+        Label1.Text = user.fullName + " ist am " + user.birthday.ToString("dd.MM.yyy") + ", dem " + dayOfYear + ". Tag des Jahres geboren und ist " + user.age.ToString() + " alt."
+        Label2.Text = "Die Person hat folgende Adresse:"
+        Label3.Text = user.street + " " + user.streetNumber
+        Label4.Text = user.postcode + " " + user.town
+        Label5.Text = "volljährig: " + user.legalAge.ToString
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    'returns the current age from the users birthday
+    Public Function GetCurrentAge(ByVal birthday As Date) As Integer
+        Dim age As Integer
+        age = Today.Year - birthday.Year
+        If (birthday > Today.AddYears(-age)) Then age -= 1
+        Return age
+    End Function
+
+End Class
