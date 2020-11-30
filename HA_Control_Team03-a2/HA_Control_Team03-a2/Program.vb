0980822f0b4a736a6acd552bba0e3a889a629a92
KevvKo <kklein915@gmail.com>
2020-11-30 23:47:27 +0100

    add comments

--- HA_Control_Team03-a2/HA_Control_Team03-a2/Program.vb
+++ HA_Control_Team03-a2/HA_Control_Team03-a2/Program.vb
@@ -5,6 +5,7 @@ Module Program
 
         Dim input As String
 
+        'Loop to restart the program'
         Do
 
             Dim i, j As Integer
@@ -12,10 +13,13 @@ Module Program
             j = 0
             Dim countPersons, remainingPersons, steps As Integer
 
+            'Inputs from the user for the algorithm'
+
             While True
                 Console.WriteLine("Bitte geben Sie die Gesamtanzahl der Personen ein:")
                 Dim inp As String = Console.ReadLine()
 
+                'Typecheck if input is an integer'
                 If (Int32.TryParse(inp, countPersons)) Then
                     Exit While
                 End If
@@ -25,6 +29,7 @@ Module Program
                 Console.WriteLine("Bitte geben Sie die Anzahl der zu verbleibenden Personen ein")
                 Dim inp As String = Console.ReadLine()
 
+                'Typecheck if input is an integer'
                 If (Int32.TryParse(inp, remainingPersons)) Then
                     Exit While
                 End If
@@ -33,13 +38,17 @@ Module Program
             While True
                 Console.WriteLine("Bitte geben Sie den Abstand der Personen untereinander ein:")
                 Dim inp As String = Console.ReadLine()
+
+                'Typecheck if input is an integer'
                 If (Int32.TryParse(inp, steps)) Then
                     Exit While
                 End If
             End While
 
+            'Contains all persons with a number'
             Dim persons = Enumerable.Range(1, countPersons).ToList()
 
+            'computing the alternative josephus problem and count the iterations'
             While persons.Count > remainingPersons
                 i = (i + steps - 1) Mod persons.Count
                 persons.RemoveAt(i)
@@ -48,13 +57,17 @@ Module Program
                 End If
             End While
 
+            'output for needed iterations and remaining persons'
             Console.WriteLine("Zum Schluss stehende Personen:")
+
             For Each person In persons
                 Console.WriteLine(person)
             Next person
+
             Console.WriteLine("Benötigte Iterationen")
             Console.WriteLine(j)
 
+            'checks, if the user should be restart the program'
             Console.WriteLine("Für einen Neustart, geben Sie bitte 'restart' ein.")
             input = Console.ReadLine()
         Loop Until Input <> "restart"
