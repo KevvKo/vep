bde13a1a6621778312b19f5462b78395270dd21a
KevvKo <kklein915@gmail.com>
2020-11-30 23:40:13 +0100

    final version for a2

--- HA_Control_Team03-a2/HA_Control_Team03-a2/Program.vb
+++ HA_Control_Team03-a2/HA_Control_Team03-a2/Program.vb
@@ -2,34 +2,61 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim i As Integer = 0
-        Dim remainingPersons As Integer = 3
-        Dim countPersons As Integer = 1000
-        Dim persons As List(Of Integer) = New List(Of Integer)()
-        Dim steps As Integer = 7
-        Dim stepCounter As Integer = steps
-        Dim length As Integer = countPersons
 
-        For j As Integer = 1 To countPersons
-            persons.Add(j)
-        Next j
-        Console.WriteLine(persons.Count Mod steps)
-        Do While persons.Count > remainingPersons
+        Dim input As String
 
-            persons.Remove(stepCounter)
-            stepCounter += steps
+        Do
 
-            If (stepCounter = (length - (length Mod steps))) Then
-                Console.WriteLine(persons.Count)
-                stepCounter = steps - (length Mod steps)
-                length = persons.Count
-                i += 1
-            End If
+            Dim i, j As Integer
+            i = 0
+            j = 0
+            Dim countPersons, remainingPersons, steps As Integer
 
-        Loop
+            While True
+                Console.WriteLine("Bitte geben Sie die Gesamtanzahl der Personen ein:")
+                Dim inp As String = Console.ReadLine()
 
-        Console.WriteLine("Benötigte Iterationen: " & CStr(i))
+                If (Int32.TryParse(inp, countPersons)) Then
+                    Exit While
+                End If
+            End While
 
+            While True
+                Console.WriteLine("Bitte geben Sie die Anzahl der zu verbleibenden Personen ein")
+                Dim inp As String = Console.ReadLine()
 
+                If (Int32.TryParse(inp, remainingPersons)) Then
+                    Exit While
+                End If
+            End While
+
+            While True
+                Console.WriteLine("Bitte geben Sie den Abstand der Personen untereinander ein:")
+                Dim inp As String = Console.ReadLine()
+                If (Int32.TryParse(inp, steps)) Then
+                    Exit While
+                End If
+            End While
+
+            Dim persons = Enumerable.Range(1, countPersons).ToList()
+
+            While persons.Count > remainingPersons
+                i = (i + steps - 1) Mod persons.Count
+                persons.RemoveAt(i)
+                If i < steps - 1 Then
+                    j += 1
+                End If
+            End While
+
+            Console.WriteLine("Zum Schluss stehende Personen:")
+            For Each person In persons
+                Console.WriteLine(person)
+            Next person
+            Console.WriteLine("Benötigte Iterationen")
+            Console.WriteLine(j)
+
+            Console.WriteLine("Für einen Neustart, geben Sie bitte 'restart' ein.")
+            input = Console.ReadLine()
+        Loop Until Input <> "restart"
     End Sub
 End Module
