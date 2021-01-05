7e75b3b730ead25c4ff4e2cd2565dd9aa50a9504
KevvKo <kklein915@gmail.com>
2021-01-05 23:55:03 +0100

    new task

---
+++ HA_OOP_Team24/HA_OOP_Team24/HA_OOP_Team24/Medikament.vb
@@ -0,0 +1,31 @@
+﻿Public Class Medikament
+    Public Zuzahlung As Double
+    Public MedikamentenID As String
+
+    Public Sub New(Zuzahlung As Double, MedikamentenID As String)
+
+        Me.Zuzahlung = Math.Round(Zuzahlung, 2)
+
+        If Me.isHexadecimal(MedikamentenID) Then
+            Me.MedikamentenID = MedikamentenID
+        End If
+
+    End Sub
+
+    Private Function isHexadecimal(value As String) As Boolean
+
+        Dim letters = New String() {"A", "B", "C", "D", "F"}
+        Dim numbers = New String() {"10", "11", "12", "13", "14", "15"}
+        value = value.ToUpper()
+
+        For i As Integer = 0 To 6
+
+            If value.Contains(letters(i)) Then
+                value = value.Replace(letters(i), numbers(i))
+            End If
+            Console.WriteLine(value)
+            Return IsNumeric(value)
+        Next
+
+    End Function
+End Class
---
+++ HA_OOP_Team24/HA_OOP_Team24/HA_OOP_Team24/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim Medikament As Medikament = New Medikament(123.1252, "111A")
+
+        Console.WriteLine(Medikament.MedikamentenID)
+    End Sub
+
+
+End Module
